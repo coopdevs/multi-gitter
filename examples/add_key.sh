ea4833a31863b539a16aab56585dbf0a46c22760
Pelayo García <pelayo.garcia@coopdevs.org>
2022-09-07 01:44:15 +0200

    feat: (wip) add new entry in YAML

--- examples/add_key.sh
+++ examples/add_key.sh
@@ -7,15 +7,18 @@
 FILE=$1
 DIR="pub_keys/"
 KEY=$DIR$(basename "$1")
+USER=$(basename "$FILE" .pub)
+NEW_SYSADMIN="
+---
+system_administrators:
+  - name: $USER
+    ssh_key: '{{ inventory_dir }}/../pub_keys/$USER.pub'
+    state: present
+"
 
 if [ ! -d "$DIR" ]; then
    echo "Destination dir is not present in repository"
    exit 2
-fi
-
-if [ -f "$KEY" ]; then
-    echo "Key is already present"
-    exit 2
 fi
 
 if [ ! -f "$FILE" ]; then
@@ -45,5 +48,21 @@ for YAML in $INVENTORY; do
   if [[ "$DIR" =~ .*"local"$ || "$YAML" == *"secrets.yml" ]]; then     
     continue
   fi
-  yq ".system_administrators" < "$YAML"
+  NAMES=$(yq "[.system_administrators.[].name] | @csv" < "$YAML")
+  for NAME in $NAMES; do
+    IFS=,
+    if [[ "$NAME" == "$USER" ]]; then
+      echo "User $USER already present in $YAML"
+      exit 2
+    else
+    echo "Adding user $USER to $YAML"
+    echo "$NEW_SYSADMIN" > new_sysadmin.yml
+    yq eval-all 'select(fileIndex == 0) *+ select(fileIndex == 1)' new_sysadmin.yml "$YAML" >> tmp.yml
+    #yq '. *= load("new_sysadmin.yml")' "$YAML"
+    rm new_sysadmin.yml "$YAML"
+    mv tmp.yml "$YAML"
+    cat "$YAML"
+    #yq -i ".system_administrators += [$NEW_SYSADMIN]" "$YAML"
+    fi
+   done
 done
